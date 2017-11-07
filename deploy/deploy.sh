a20f671e3de8356018a48cfc84da5ae25a004604
Anton Nemtsev <thesilentimp@gmail.com>
2017-11-08 00:56:47 +0200

    lock

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,8 @@ export PROCESS_NAME=service.frontender.info
 
 mkdir ${RESULT_DIR}
 shopt -s extglob
-mv -f !(${RESULT_DIR}) ./${RESULT_DIR}
+mv -f -v !(${RESULT_DIR}) ./${RESULT_DIR}
+mv -f -v ./{.[!.],}* ./${RESULT_DIR}
 tar -czf ${ARCH_NAME} ${RESULT_DIR}
 sshpass -e scp -C -o StrictHostKeyChecking=no ${ARCH_NAME} ${SSH_USER}@${SSH_IP}:${WEB_PATH}
 sshpass -e ssh -C ${SSH_USER}@${SSH_IP} << EOF
@@ -17,6 +18,7 @@ cd ${WEB_PATH};
 tar -xzf ./${ARCH_NAME} -C ./;
 rm ./${ARCH_NAME};
 ls -dt ${WEB_PATH}*/ | tail -n +5 | xargs rm -rf;
+rm ./.env
 if [ ! -f ".env" ]; then
     echo WEB_PATH=${WEB_PATH} >> .env;
     echo GITHUB_SECRET_TOKEN=${GITHUB_SECRET_TOKEN} >> .env;
@@ -28,9 +30,9 @@ if [ ! -f ".env" ]; then
     echo TWITTER_TOKEN_SECRET=${TWITTER_TOKEN_SECRET} >> .env;
 fi
 cd ${RESULT_DIR};
-nvm install 8;
-nvm use 8;
-nvm alias default 8;
+nvm install 8.9;
+nvm use 8.9;
+nvm alias default 8.9;
 npm install;
 npm run build;
 cd ..
